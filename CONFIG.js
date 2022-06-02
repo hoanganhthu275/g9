fe18fe7c8ba3b8388ce37df63614c6f5fb9998f8
hoanganhthu275 <106757366+hoanganhthu275@users.noreply.github.com>
2022-06-02 21:47:42 +0700

    Initial commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Sweet gift đến từ người xinh đẹp nhất thế giới",
+  descriptionHeader:
+    "Từ giờ ngủ sớm ít nhất 23h30 đi ngủ rùi nhớ chưa?",
+  buttonYes: "Ngủ Ngon nhé",
+  buttonNo: "Ngủ sớm hơn luôn",
+  titleModar: "Không ngủ đủ giấc đúng giờ là ăn đấm đấy",
+  descriptionModar:
+    'Tôi tin tưởng bạn làm được mà ^^ </br> Ngủ ngon và mơ về tôi nhé :))) </br> "Chúc bạn mơ đẹp. </br>  Mít ziuu "',
+};
